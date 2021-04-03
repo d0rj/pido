8d40e5547db1feeef540a30d66869b2b09b240de
d0rj <hanaan@internet.ru>
2021-04-03 16:46:37 +0300

    Some changes in first file.

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,14 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
+#' hello: package to deal with good and bad bees
+#'
+#' Extremely important package for every Bear
+#'
+#' I've written this package a long time ago in a far-far galaxy.
+#'
+#' @name hello
+#' @docType package
 hello <- function() {
   print("Hello, world!")
 }

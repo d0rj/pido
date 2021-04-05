98f897225f7e472f57326a797f3f46bedac1a8be
d0rj <hanaan@internet.ru>
2021-04-05 12:00:59 +0300

    Added annotations for function in text.R

--- R/text.R
+++ R/text.R
@@ -1,6 +1,15 @@
 library(stringr)
 
 
+#' Returns n-gramms for given vector of some elements
+#'
+#' @param tokens Vector of elements to be n-grammed
+#' @param n Size of n-gramm
+#' @return List of vector of n-grammed elements
+#' @export
+#' @examples
+#' vec <- c(1, 2, 3, 4, 5)
+#' nGramm(vec, 3)
 nGramm <- function(tokens, n) {
   stopifnot(is.vector(tokens))
   stopifnot(is.numeric(n))
@@ -18,13 +27,22 @@ nGramm <- function(tokens, n) {
 }
 
 
+#' Returns n-gramms of words for given input string
+#'
+#' @param input Input string
+#' @param n Size of n-gramm
+#' @param clearData Boolean - should function clear input data (remove all useless characters)
+#' @return List of vector of words
+#' @export
+#' @examples
+#' wordNGramm('Never gonna give you up Never gonna let you down', 3)
 wordNGramm <- function(input, n, clearData = TRUE) {
   stopifnot(is.character(input))
   stopifnot(is.numeric(n))
   stopifnot(is.logical(clearData))
 
   if (clearData) {
-    input <- str_replace(input, '[^a-zA-Z0-9 ]', ' ')
+    input <- str_replace(input, '[^a-zA-Z0-9а-яА-Я ]', ' ')
   }
   tokens <- strsplit(input, split=' ', fixed=TRUE)[[1]]
 
@@ -32,6 +50,15 @@ wordNGramm <- function(input, n, clearData = TRUE) {
 }
 
 
+#' Returns n-gramms of characters for given input string
+#'
+#' @param input Input string
+#' @param n Size of n-gramm
+#' @param strings Boolean - should function return vector of string n-gramms. FALSE - return list of vectors of characters
+#' @return Vector of strings or List of vector of characters @seealso strings
+#' @export
+#' @examples
+#' symbolNGramm('hello world', 4)
 symbolNGramm <- function(input, n, strings = TRUE) {
   stopifnot(is.character(input))
   stopifnot(is.numeric(n))
